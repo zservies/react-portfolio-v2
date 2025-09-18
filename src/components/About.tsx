b70d6437d587d3c92050f21c009182a0afdc6469
Zach Servies <zachservies@gmail.com>
2025-09-17 22:49:33 -0400

    About component basics.

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import './About.css';
+
+export default function About() {
+  return (
+    <div className='about-wrapper'>
+      <div className='about-picture'>placeholder for image</div>
+      <div className='about-text'>
+        <h2>About </h2>
+        <p>Hello this is some text about me. Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, provident quas iste facere minima similique veniam placeat sit assumenda dolores temporibus exercitationem neque rerum voluptas non beatae mollitia magni. Voluptatem?</p>
+      </div>
+    </div>
+  )
+}
--- src/components/CenterStage.tsx
+++ src/components/CenterStage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './CenterStage.css';
 import Nameplate from './Nameplate';
+import About from './About';
 
 export default function CenterStage() {
   return (
     <div className='center-stage-wrapper'>
       <Nameplate />
+      <About />
     </div>
   )
 }
